4a848269683ab57fbcae1bdd4ccce3db3144aaea
Aaditya Shah <aadishah1020@gmail.com>
2025-04-23 00:16:54 -0400

    Learned to perform mongoDB queries in mongoose

--- index.js
+++ index.js
@@ -1,8 +1,82 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
-const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = 8080;
 
-app.set('view engine', 'ejs')
+//main().then(result => console.log(result)).catch(err => console.log(err));
+
+try {
+    await main();
+    console.log("Sucessfully connected🚀 and all operations performed");
+} catch (err) {
+    console.log(err.message);
+}
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/sarathi');
+  console.log("Successful Connection🚀");
+
+  //creating a schema
+const userSchema = new mongoose.Schema(
+    {
+        name: String,
+        age: Number,
+        email: String
+    }
+);
+
+//model - a toolbox by mongoose to create,edit,delete,update,read documents
+const User = mongoose.model("User", userSchema);
+
+try {
+    await User.deleteMany({});
+    console.log("everything deleted");
+} catch(err) {
+    console.log(err);
+}
+
+//to insert 1 document into the collection "users"
+//_id will be created automatically which will be a PRIMARY KEY - UNIQUE and NOT NULL
+const user1 = new User(
+    {
+        name: "Aaditya",
+        age: 19,
+        email: "aadityashah2074@gmail.com"
+    }
+);
+
+await user1.save(); //this will save this document "user" in the database's collection
+
+//to insert many documents - isn't used much because at a time we only register one user in
+const users = User.insertMany(
+    [
+        {
+            name: "Swechha",
+            age: 29,
+            email: "swchha@gmail.com"
+        },
+        {
+            name: "Regmi",
+            age: 10,
+            email: "regmi@gmail.com"
+        },
+        {
+            name: "Shah",
+            age: 39,
+            email: "aadishah@gmail.com"
+        }   
+    ]
+).then(res => console.log(res)).catch(err => console.log(err)); //you don't need to save individually and this returns a promise
+
+//model also has a similar find method but it returns a query object and not a promise but we can use then and catch methods
+  
+  
+
+
+}
+
+
+
+
+
+
+
+
+
--- prac.js
+++ prac.js
@@ -1,15 +1,41 @@
-new Promise((resolve, reject) => {
-    setTimeout(resolve("Step 1 ✅"),10000);
-  })
-    .then((result1) => {
-      console.log(result1); // Step 1 ✅
-      return "Step 2 ✅";
-    })
-    .then((result2) => {
-      console.log(result2); // Step 2 ✅
-      return "Step 3 ✅";
-    })
-    .then((result3) => {
-      console.log(result3); // Step 3 ✅
-    });
-  
+let internetConnection = Math.floor(Math.random() * 6);
+
+let prom = new Promise((resolve, reject) => {
+    console.log("Inside promise")
+    setTimeout(() => {
+        if(internetConnection > 2) {
+            console.log("Hii"); //some tasks here
+            resolve("Connection successful"); //resolved 
+        }
+        else {
+            reject("Failed slow internet");
+        }
+    },2000);
+
+});
+
+//handling the promise
+// prom.then(results => console.log(`Your result is: ${results}`))
+//     .catch (err => console.log(`Error caught: ${err}`));
+
+
+//lets try this same using async/await
+
+async function saveToDb() {
+    try {
+        let result = await prom;
+        console.log(`Your result is: ${result}`);
+    } catch(err) {
+        console.log("caught: ", err);
+    }
+}
+
+saveToDb();
+
+function letsDo (name) {
+    for(let i = 0; i < 1000000; i++) {
+        console.log(name, i);
+    }
+}
+
+letsDo("Aaditya Loves Swechha a lot🥺❤️");
