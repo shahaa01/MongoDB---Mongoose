70b3c9d11dcb898c21ecae8198a50df89b7c311c
Aaditya Shah <aadishah1020@gmail.com>
2025-04-23 18:14:02 -0400

    Completed mongoose learning

--- index.js
+++ index.js
@@ -1,82 +1,59 @@
 const mongoose = require('mongoose');
 
-//main().then(result => console.log(result)).catch(err => console.log(err));
-
-try {
+(async function () {
+  try {
     await main();
-    console.log("Sucessfully connected🚀 and all operations performed");
-} catch (err) {
-    console.log(err.message);
-}
+    console.log("Successfully connected and all operations performed");
+  } catch (err) {
+    console.error("Outer error:", err.message);
+  }
+})();
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/sarathi');
-  console.log("Successful Connection🚀");
+  console.log("🚀 Connected to DB");
 
-  //creating a schema
-const userSchema = new mongoose.Schema(
-    {
-        name: String,
-        age: Number,
-        email: String
-    }
-);
+  const userSchema = new mongoose.Schema({
+    name: {type: String, required: true, minlenngth}
+  });
 
-//model - a toolbox by mongoose to create,edit,delete,update,read documents
-const User = mongoose.model("User", userSchema);
+  const User = mongoose.model("User", userSchema);
 
-try {
+  try {
     await User.deleteMany({});
-    console.log("everything deleted");
-} catch(err) {
-    console.log(err);
+    console.log("🧹 Users deleted");
+
+    const user1 = new User({
+      name: "Aaditya",
+      age: 19,
+      email: "aadityashah2074@gmail.com"
+    });
+    await user1.save();
+
+    await User.insertMany([
+      { name: "Swechha", age: 29, email: "swchha@gmail.com" },
+      { name: "Regmi", age: 10, email: "regmi@gmail.com" },
+      { name: "Shah", age: 39, email: "aadishah@gmail.com" }
+    ]);
+    console.log("📦 Users inserted");
+
+    const offerUsers = await User.find({ age: { $gt: 16 } }, { email: 1, age: 1 });
+    console.log("Offer users:", offerUsers);
+
+    await User.updateMany({age: {$gte: 18}}, {isEligible: true});
+    /**
+     * If you try to write a field that isn’t defined in the 
+     * schema, Mongoose will ignore it silently during .save() 
+     * or .update().
+
+     */
+    //User.find is an async function so you will have to await and then print it
+    console.log(await User.find({}));
+  } catch (err) {
+    console.error("Error is: " + err.message); //the error will be handled here and the outer catch will not handle it
+    //if you want the outer catch to handle it, you can throw the error from here - throw err;
+  }
+
+  //schema validations
+  //min,max,enums,custom error messages, accessing those custom messages directly - refer to notes
 }
-
-//to insert 1 document into the collection "users"
-//_id will be created automatically which will be a PRIMARY KEY - UNIQUE and NOT NULL
-const user1 = new User(
-    {
-        name: "Aaditya",
-        age: 19,
-        email: "aadityashah2074@gmail.com"
-    }
-);
-
-await user1.save(); //this will save this document "user" in the database's collection
-
-//to insert many documents - isn't used much because at a time we only register one user in
-const users = User.insertMany(
-    [
-        {
-            name: "Swechha",
-            age: 29,
-            email: "swchha@gmail.com"
-        },
-        {
-            name: "Regmi",
-            age: 10,
-            email: "regmi@gmail.com"
-        },
-        {
-            name: "Shah",
-            age: 39,
-            email: "aadishah@gmail.com"
-        }   
-    ]
-).then(res => console.log(res)).catch(err => console.log(err)); //you don't need to save individually and this returns a promise
-
-//model also has a similar find method but it returns a query object and not a promise but we can use then and catch methods
-  
-  
-
-
-}
-
-
-
-
-
-
-
-
-
